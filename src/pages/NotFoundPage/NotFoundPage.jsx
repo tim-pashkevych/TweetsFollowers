6762c44be02c4f2e8a027409ae19111de9046d80
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-02-26 02:18:27 +0200

    Not Found 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,17 @@
-import React from "react"
+import logoImg from "@/assets/images/logo.png"
+import styles from "./NotFoundPage.module.css"
+import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
-  return <div>NotFound</div>
+  return (
+    <div className={styles.container}>
+      <img src={logoImg} alt='Logo image' width={200} height={200} />
+      <h1>Nothing found</h1>
+      <Link to='/' className={styles.button}>
+        Go to Home page
+      </Link>
+    </div>
+  )
 }
 
 export default NotFoundPage
